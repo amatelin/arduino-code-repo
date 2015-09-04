f08997731784e77ab8d306cb6dfca79744037365
Alexis Matelin <a.matelin@gmail.com>
2015-09-04 17:13:07 -0400

    Changed pin number in esp_serial_com arduino sketch

--- esp_8266/esp8266_serial_com/esp8266_serial_com.ino
+++ esp_8266/esp8266_serial_com/esp8266_serial_com.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
  
-SoftwareSerial esp8266(8,9); // make RX Arduino line is pin 2, make TX Arduino line is pin 3.
+SoftwareSerial esp8266(2,3); // make RX Arduino line is pin 2, make TX Arduino line is pin 3.
                              // This means that you need to connect the TX line from the esp to the Arduino's pin 2
                              // and the RX line from the esp to the Arduino's pin 3
 void setup()
